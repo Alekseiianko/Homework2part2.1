9f5002a3ca6c28dbda3532bc3d8212f309d2faea
Alex Yanko <esoesfiit@icloud.com>
2020-04-26 19:30:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework2part2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.homework2part2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonBack;
+    private Button buttonGo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init() {
+        buttonBack = findViewById(R.id.button1);
+        buttonGo = findViewById(R.id.button2);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        buttonGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
