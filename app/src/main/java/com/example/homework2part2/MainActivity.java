54df80ddace41555b98c88207122a773ebda8651
Alex Yanko <esoesfiit@icloud.com>
2020-04-27 19:25:18 +0300

    Correct commit

--- app/src/main/java/com/example/homework2part2/MainActivity.java
+++ app/src/main/java/com/example/homework2part2/MainActivity.java
@@ -8,23 +8,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonBack;
     private Button buttonGo;
+    private TextView text;
+    private int randomNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        text = findViewById(R.id.adressFoto);
+        Random random = new Random();
+        randomNumber = random.nextInt(100-1) + 1;
+        text.setText("http://myfile.org/" + randomNumber);
     }
 
     public void init() {
         buttonBack = findViewById(R.id.button1);
         buttonGo = findViewById(R.id.button2);
+        text = findViewById(R.id.adressFoto);
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +44,9 @@ public class MainActivity extends AppCompatActivity {
         buttonGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+
             }
         });
     }
